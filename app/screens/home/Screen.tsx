ee8662ca8016f851e0c896ceb1a5b7baea4ade50
adasilvapdev <adasilvapdev@gmail.com>
2021-06-30 17:47:37 -0400

    Deleted unused elements

--- app/screens/home/Screen.tsx
+++ app/screens/home/Screen.tsx
@@ -8,18 +8,6 @@ const Screen = (props: any) => {
     //* Using the custom hook we made to pull the theme colors
     const { colors, isDark } = useTheme();
 
-    // const containerStyle = {
-    //     flex: 1,
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     /* 
-    //     * the colors.background value will change dynamicly with
-    //     * so if we wanna change its value we can go directly to the pallet
-    //     * this will make super easy to change and maintain mid or end project
-    //     */
-    //     backgroundColor: colors.background,
-    // };
-
     const styles = StyleSheet.create({
         containerStyle: {
             flex: 1,
